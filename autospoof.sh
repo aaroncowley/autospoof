e3ee0484f1510d3503104c40220baf2476132bb9
root <root@Ubuntu1604.boltcorp.com>
2018-11-28 14:02:11 -0600

    changed target to rc.local instead of cron

--- autospoof.sh
+++ autospoof.sh
@@ -1,18 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root"
     exit 1
 fi
 
-cd /root
 
 if [ ! -d "/root/portspoof" ]; then
     # Control will enter here if portspoof doesn't exist.
-    git clone https://github.com/drk1wi/portspoof.git && cd portspoof
-    ./configure 
-    make
-    make install
+    git clone https://github.com/drk1wi/portspoof.git /root
+    /root/portspoof/configure 
+    /root/portspoof/make
+    /root/portspoof/make install
 fi
 
 iptables --table nat -F
@@ -24,13 +23,14 @@ for prange in ${spoofed}; do
     iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport ${prange} -j REDIRECT --to-ports 4444
 done
 
-portspoof -c /root/portspoof/tools/portspoof.conf -s /root/portspoof/tools/portspoof_signatures -D
+/usr/local/bin/portspoof -c /root/portspoof/tools/portspoof.conf -s /root/portspoof/tools/portspoof_signatures -D
+
 iptables --table nat --list
 
-if grep -q /root/autospoof.sh /var/spool/cron/crontabs/root; then
-    echo already in crontab
+if grep -q /root/autospoof.sh /etc/rc.local; then
+    echo already in rc.local
 else
-    (crontab -l ; echo "@reboot /root/autospoof.sh") | crontab -
+    echo "/bin/bash /root/autospoof.sh" >> /etc/rc.local
 fi
 
 echo "All DONE"
